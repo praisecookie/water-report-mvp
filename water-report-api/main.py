089e33769daacd79e2c66f47443308099ca28e87
praisecookie <praisecookie3@gmail.com>
2026-02-27 12:43:45 +0100

    Update CORS for Vercel deployment

--- water-report-api/main.py
+++ water-report-api/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Water Workforce API")
 # Allow Next.js frontend to talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
